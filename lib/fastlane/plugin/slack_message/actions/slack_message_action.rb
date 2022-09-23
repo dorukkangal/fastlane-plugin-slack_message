454dfeb2e2df84ad1fb18161b3dfc178ead855c7
Doruk Kangal <dorukkangal@gmail.com>
2022-09-23 12:32:32 +0300

    Refactor code

--- lib/fastlane/plugin/slack_message/actions/slack_message_action.rb
+++ lib/fastlane/plugin/slack_message/actions/slack_message_action.rb
@@ -1,148 +1,104 @@
 require 'fastlane/action'
-require 'fastlane/actions/slack'
+require 'fastlane_core/configuration/config_item'
 require_relative '../helper/slack_message_helper'
 
 module Fastlane
   module Actions
     class SlackMessageAction < Action
-      class Runner
-        def initialize(slack_url)
-          @webhook_url = slack_url
-
-          @client = Faraday.new do |conn|
-            conn.use(Faraday::Response::RaiseError)
-          end
-        end
-
-        def run(options)
-
-          options[:message] = Fastlane::Actions::SlackAction.trim_message(options[:message].to_s || '')
-          options[:message] = Fastlane::Notification::Slack::LinkConverter.convert(options[:message])
-
-          options[:pretext] = options[:pretext].gsub('\n', "\n") unless options[:pretext].nil?
-
-          channel = nil
-          if options[:channel].to_s.length > 0
-            channel = options[:channel]
-            channel = ('#' + options[:channel]) unless ['#', '@'].include?(channel[0]) # send message to channel by default
-          end
-
-          username = options[:use_webhook_configured_username_and_icon] ? nil : options[:username]
-
-          slack_attachment = Fastlane::Actions::SlackAction.generate_slack_attachments(options)
-          link_names = options[:link_names]
-          icon_url = options[:use_webhook_configured_username_and_icon] ? nil : options[:icon_url]
-
-          post_message(
-            channel: channel,
-            thread_timestamp: options[:thread_timestamp],
-            username: username,
-            attachments: [slack_attachment],
-            link_names: link_names,
-            icon_url: icon_url,
-            fail_on_error: options[:fail_on_error]
-          )
-        end
-
-        def post_message(channel:, thread_timestamp:, username:, attachments:, link_names:, icon_url:, fail_on_error:)
-          @client.post(@webhook_url) do |request|
-            request.headers['Content-Type'] = 'application/json'
-            request.body = {
-              channel: channel,
-              username: username,
-              thread_ts: thread_timestamp,
-              icon_url: icon_url,
-              attachments: attachments,
-              link_names: link_names
-            }.to_json
-          end
-          UI.success('Successfully sent Slack notification')
-        rescue => error
-          UI.error("Exception: #{error}")
-          message = "Error pushing Slack message, maybe the integration has no permission to post on this channel? Try removing the channel parameter in your Fastfile, this is usually caused by a misspelled or changed group/channel name or an expired SLACK_URL"
-          if fail_on_error
-            UI.user_error!(message)
-          else
-            UI.error(message)
-          end
-        end
-      end
-
       def self.run(options)
-        Runner.new(options[:slack_url]).run(options)
+        channel = options[:channel]
+        channel = Helper::SlackMessageHelper.format_channel(channel)
+
+        username = options[:use_webhook_configured_username_and_icon] ? nil : options[:username]
+
+        message = Helper::SlackMessageHelper.prepare_message(options)
+
+        link_names = options[:link_names]
+        icon_url = options[:use_webhook_configured_username_and_icon] ? nil : options[:icon_url]
+
+        Helper::SlackMessageHelper.post_message(
+          slack_url: options[:slack_url],
+          channel: channel,
+          thread_timestamp: options[:thread_timestamp],
+          username: username,
+          attachments: [message],
+          link_names: link_names,
+          icon_url: icon_url,
+          fail_on_error: options[:fail_on_error]
+        )
       end
 
       def self.available_options
         [
           FastlaneCore::ConfigItem.new(key: :message,
-                                       env_name: "FL_SLACK_MESSAGE",
-                                       description: "The message that should be displayed on Slack. This supports the standard Slack markup language",
+                                       env_name: 'FL_SLACK_MESSAGE',
+                                       description: 'The message that should be displayed on Slack. This supports the standard Slack markup language',
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :pretext,
-                                       env_name: "FL_SLACK_PRETEXT",
-                                       description: "This is optional text that appears above the message attachment block. This supports the standard Slack markup language",
+                                       env_name: 'FL_SLACK_PRETEXT',
+                                       description: 'This is optional text that appears above the message attachment block. This supports the standard Slack markup language',
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :channel,
-                                       env_name: "FL_SLACK_CHANNEL",
-                                       description: "#channel or @username",
+                                       env_name: 'FL_SLACK_CHANNEL',
+                                       description: '#channel or @username',
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :thread_timestamp,
-                                       env_name: "FL_SLACK_THREAD_TIMESTAMP",
-                                       description: "Timestamp of the message to reply",
+                                       env_name: 'FL_SLACK_THREAD_TIMESTAMP',
+                                       description: 'Timestamp of the message to reply',
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :slack_url,
-                                       env_name: "SLACK_URL",
+                                       env_name: 'SLACK_URL',
                                        sensitive: true,
-                                       description: "Create an Incoming WebHook for your Slack group",
+                                       description: 'Create an Incoming WebHook for your Slack group',
                                        verify_block: proc do |value|
-                                         UI.user_error!("Invalid URL, must start with https://") unless value.start_with?("https://")
+                                         UI.user_error!('Invalid URL, must start with https://') unless value.start_with?('https://')
                                        end),
           FastlaneCore::ConfigItem.new(key: :username,
-                                       env_name: "FL_SLACK_USERNAME",
-                                       description: "Overrides the webhook's username property if use_webhook_configured_username_and_icon is false",
-                                       default_value: "fastlane",
+                                       env_name: 'FL_SLACK_USERNAME',
+                                       description: 'Overrides the webhook\'s username property if use_webhook_configured_username_and_icon is false',
+                                       default_value: 'fastlane',
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :use_webhook_configured_username_and_icon,
-                                       env_name: "FL_SLACK_USE_WEBHOOK_CONFIGURED_USERNAME_AND_ICON",
-                                       description: "Use webhook's default username and icon settings? (true/false)",
+                                       env_name: 'FL_SLACK_USE_WEBHOOK_CONFIGURED_USERNAME_AND_ICON',
+                                       description: 'Use webhook\'s default username and icon settings? (true/false)',
                                        default_value: false,
                                        type: Boolean,
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :icon_url,
-                                       env_name: "FL_SLACK_ICON_URL",
-                                       description: "Overrides the webhook's image property if use_webhook_configured_username_and_icon is false",
-                                       default_value: "https://fastlane.tools/assets/img/fastlane_icon.png",
+                                       env_name: 'FL_SLACK_ICON_URL',
+                                       description: 'Overrides the webhook\'s image property if use_webhook_configured_username_and_icon is false',
+                                       default_value: 'https://fastlane.tools/assets/img/fastlane_icon.png',
                                        optional: true),
           FastlaneCore::ConfigItem.new(key: :payload,
-                                       env_name: "FL_SLACK_PAYLOAD",
-                                       description: "Add additional information to this post. payload must be a hash containing any key with any value",
+                                       env_name: 'FL_SLACK_PAYLOAD',
+                                       description: 'Add additional information to this post. payload must be a hash containing any key with any value',
                                        default_value: {},
                                        type: Hash),
           FastlaneCore::ConfigItem.new(key: :default_payloads,
-                                       env_name: "FL_SLACK_DEFAULT_PAYLOADS",
-                                       description: "Specifies default payloads to include. Pass an empty array to suppress all the default payloads",
-                                       default_value: ['lane', 'test_result', 'git_branch', 'git_author', 'last_git_commit', 'last_git_commit_hash'],
+                                       env_name: 'FL_SLACK_DEFAULT_PAYLOADS',
+                                       description: 'Specifies default payloads to include. Pass an empty array to suppress all the default payloads',
+                                       default_value: %w[lane test_result git_branch git_author last_git_commit last_git_commit_hash],
                                        type: Array),
           FastlaneCore::ConfigItem.new(key: :attachment_properties,
-                                       env_name: "FL_SLACK_ATTACHMENT_PROPERTIES",
-                                       description: "Merge additional properties in the slack attachment, see https://api.slack.com/docs/attachments",
+                                       env_name: 'FL_SLACK_ATTACHMENT_PROPERTIES',
+                                       description: 'Merge additional properties in the slack attachment, see https://api.slack.com/docs/attachments',
                                        default_value: {},
                                        type: Hash),
           FastlaneCore::ConfigItem.new(key: :success,
-                                       env_name: "FL_SLACK_SUCCESS",
-                                       description: "Was this build successful? (true/false)",
+                                       env_name: 'FL_SLACK_SUCCESS',
+                                       description: 'Was this build successful? (true/false)',
                                        optional: true,
                                        default_value: true,
                                        type: Boolean),
           FastlaneCore::ConfigItem.new(key: :fail_on_error,
-                                       env_name: "FL_SLACK_FAIL_ON_ERROR",
-                                       description: "Should an error sending the slack notification cause a failure? (true/false)",
+                                       env_name: 'FL_SLACK_FAIL_ON_ERROR',
+                                       description: 'Should an error sending the slack notification cause a failure? (true/false)',
                                        optional: true,
                                        default_value: true,
                                        type: Boolean),
           FastlaneCore::ConfigItem.new(key: :link_names,
-                                       env_name: "FL_SLACK_LINK_NAMES",
-                                       description: "Find and link channel names and usernames (true/false)",
+                                       env_name: 'FL_SLACK_LINK_NAMES',
+                                       description: 'Find and link channel names and usernames (true/false)',
                                        optional: true,
                                        default_value: false,
                                        type: Boolean)
@@ -158,15 +114,15 @@ module Fastlane
       end
 
       def self.description
-        "Send a message to your [Slack](https://slack.com) group"
+        'Send a message to your [Slack](https://slack.com) group'
       end
 
       def self.details
-        "This plugin is forked from the Fastlane Slack action. Additionally, it takes thread_timestamp parameter to send the message under a thread."
+        'This plugin is forked from the Fastlane Slack action. Additionally, it takes thread_timestamp parameter to send the message under a thread.'
       end
 
       def self.authors
-        ["Doruk Kangal"]
+        ['Doruk Kangal']
       end
 
       def self.example_code
--- lib/fastlane/plugin/slack_message/helper/slack_message_helper.rb
+++ lib/fastlane/plugin/slack_message/helper/slack_message_helper.rb
@@ -1,15 +1,68 @@
 require 'fastlane_core/ui/ui'
+require 'fastlane/action'
+require 'fastlane/actions/slack'
+require 'faraday'
 
 module Fastlane
-  UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
+  UI = FastlaneCore::UI unless Fastlane.const_defined?(:UI)
 
   module Helper
     class SlackMessageHelper
-      # class methods that you define here become available in your action
-      # as `Helper::SlackMessageHelper.your_method`
-      #
-      def self.show_message
-        UI.message("Hello from the slack_message plugin helper!")
+      @client = Faraday.new do |builder|
+        builder.use(Faraday::Response::RaiseError)
+      end
+
+      def self.post_message(slack_url:, channel:, thread_timestamp:, username:, attachments:, link_names:, icon_url:, fail_on_error:)
+        @client.post(slack_url) do |request|
+          request.headers['Content-Type'] = 'application/json'
+          request.body = {
+            channel: channel,
+            username: username,
+            thread_ts: thread_timestamp,
+            icon_url: icon_url,
+            attachments: attachments,
+            link_names: link_names
+          }.to_json
+        end
+        UI.success('Successfully sent Slack notification')
+      rescue StandardError => e
+        UI.error("Error while pushing Slack message: #{e}")
+        message = "Maybe the integration has no permission to post on this channel? Try removing the channel parameter in your Fastfile, this is usually caused by a misspelled or changed group/channel name or an expired SLACK_URL"
+        if fail_on_error
+          UI.user_error!(message)
+        else
+          UI.error(message)
+        end
+      end
+
+      def self.prepare_message(options)
+        options[:message] = trim(options[:message])
+        options[:message] = convert_links(options[:message])
+        options[:pretext] = newline_interpretation(options[:pretext])
+
+        Fastlane::Actions::SlackAction.generate_slack_attachments(options)
+      end
+
+      def self.trim(text)
+        Fastlane::Actions::SlackAction.trim_message(text || '')
+      end
+
+      def self.convert_links(text)
+        Fastlane::Notification::Slack::LinkConverter.convert(text)
+      end
+
+      def self.newline_interpretation(text)
+        (text || '').gsub('\n', "\n")
+      end
+
+      def self.format_channel(channel_name)
+        if channel_name.nil? || channel_name.empty?
+          nil
+        elsif %w[# @].include?(channel_name[0])
+          channel_name
+        else
+          '#' + channel_name
+        end
       end
     end
   end
